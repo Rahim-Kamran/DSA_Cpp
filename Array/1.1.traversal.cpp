ed248854a4f93b36d89de2c906f26aeb5bcb69cb
Rahim Kamran <khanrahim007k@gmail.com>
2026-02-02 12:17:40 +0530

    Updates in array

--- Array/1.1.traversal.cpp
+++ Array/1.1.traversal.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <climits>
 using namespace std;
+//input and output of array
 /*int main(){
     int a[10],n,i;
     cout<<"Enter the size of array: ";
@@ -14,6 +16,7 @@ using namespace std;
 
     }
 }
+    //input and output of array
 int main()
 {
     int a[10], n, i;
@@ -28,7 +31,8 @@ int main()
     {
         cout << a[i] << " ";
     }
-}*/
+}
+    //input and output of array
 int main()
 {
     int a[20], n, i;
@@ -44,4 +48,62 @@ int main()
     {
         cout << a[i] << " ";
     }
+}*/
+//input and output of array
+// int main()
+//     {
+//         // int size = 5;
+//         int marks[5];
+//         int size = 5;
+//         // cout<< sizeof(marks) / sizeof(int)<<endl;
+       
+//         for(int i =0;i<size;i++){
+//             cin>>marks[i];
+//         }
+//         cout<<"The marks are: ";
+//         for(int i=0;i<size;i++){
+//             cout<<marks[i]<<" ";
+//         }
+
+
+//     }
+
+//find the smallest number in an array
+// int main()
+// {
+//     int n[10];
+//     int smallest = INT_MAX;
+//     int size;
+//     cout << "Enter the size of array: ";
+//     cin >> size;
+//     cout << "Enter the elements of array: ";
+//     for (int i = 0; i < size; i++)
+//     {
+//         cin >> n[i];
+//         if (n[i] < smallest)
+//         {
+//             smallest = n[i];
+//         }
+//     }
+//     cout << "Smallest element is: " << smallest << endl;
+// }
+//find the largest number in an array
+int main()
+{
+    int n[10];
+    int largets = INT_MAX;
+    int size;
+    // cout<<"Enter the size of array: ";
+    // cin>>size;
+    // cout<<"Enter the elements of array: ";
+    for(int i=0;i<size;i++){
+        //Shortest way to take input
+        // cin>>n[i];
+        // if(n[i]>largets){
+        //     largets=n[i]; 
+        largets = min(largets, n[i]);
+        cin>>n[i];
+        }
+// }
+    cout<<"Largest element is: "<<largets<<endl;
 }
