7e990e150ddc26ae4130c0c9ffb26f6cfea6654c
Rahim Kamran <khanrahim007k@gmail.com>
2026-04-24 13:36:50 +0530

    Quatric probing

--- Array/9.1.hasing.cpp
+++ Array/9.1.hasing.cpp
@@ -7,6 +7,7 @@ const int N = 10;
 int hashTable[N];
 void insert(int key){
     int index = key % N;
+    //Linear probing to resolve collision
     while(hashTable[index] != 0){
         index = (index + 1) % N; 
     }
@@ -31,3 +32,38 @@ int main(){
     }
     return 0;
 }
+
+// //Quatric probing
+// #include <iostream>
+// using namespace std;
+// const int N = 10;
+// int hashTable[N];
+// void insert(int key){
+//     int index = key % N;
+//     int i = 0;
+//     //Quadratic probing to resolve collision
+//     while(hashTable[index] != 0){
+//         i++;
+//         index = (key + i*i) % N; 
+//     }
+//     hashTable[index] = key;
+// }
+// int main(){
+//     for(int i=0; i<N; i++){
+//         hashTable[i] = 0; 
+//     }
+//     insert(15);
+//     insert(25);
+//     insert(35);
+//     insert(5);
+//     insert(10);
+//     cout<<"Hash Table: "<<endl;
+//     for(int i=0; i<N; i++){
+//         if(hashTable[i] != 0){
+//             cout<<"Index "<<i<<": "<<hashTable[i]<<endl;
+//         } else {
+//             cout<<"Index "<<i<<": Empty"<<endl;
+//         }
+//     }
+//     return 0;
+// }
